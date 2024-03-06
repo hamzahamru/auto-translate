39296cc3e88ecbbdf5b022b30f8923e8b752cd85
Hamzah <55149999+hamzahamru@users.noreply.github.com>
2024-03-06 19:00:10 +0700

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,25 @@
+const readlineSync = require('readline-sync');
+const translate = require('translate-google');
+const autofill = require('@hamzahamru/autofill-typeform');
+const spafind = require('@hamzahamru/spafind');
+const translates = (text, from, to) => new Promise((resolve, reject) => {
+    translate(text, {
+        from: from,
+        to: to
+    }).then(res => {
+        resolve(res)
+    }).catch(err => {
+        reject(err)
+    });
+});
+
+(async () => {
+    const from = readlineSync.question('Translate from (ex. en/id) : ');
+    const to = readlineSync.question('Translate to (ex. en/id) : ');
+    while (true) {
+    const text = readlineSync.question('Input text to translate : ');
+    const translate = await translates(text);
+    console.log(`Output text to translate: ${translate}`);
+    console.log('')
+    }
+})();
